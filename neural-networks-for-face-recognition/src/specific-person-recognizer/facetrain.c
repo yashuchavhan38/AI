be635c3e0fa603667835927b82df4e587c2ede7a
lurenjia <yanshengjia1995@163.com>
2016-04-20 18:18:58 +0800

    Add neural-networks-for-face-recognition

---
+++ neural-networks-for-face-recognition/src/specific-person-recognizer/facetrain.c
@@ -0,0 +1,422 @@
+/* 特定任务识别器
+ ******************************************************************
+ * HISTORY
+ * 15-Oct-94  Jeff Shufelt (js), Carnegie Mellon University
+ *      Prepared for 15-681, Fall 1994.
+ *
+ * Tue Oct  7 08:12:06 EDT 1997, bthom, added a few comments,
+ *       tagged w/bthom
+ *
+ * Modified by Shengjia Yan @2016-03-29
+ *
+ ******************************************************************
+ */
+
+/*
+██████  ███████ ██████  ███████  ██████  ███    ██
+██   ██ ██      ██   ██ ██      ██    ██ ████   ██
+██████  █████   ██████  ███████ ██    ██ ██ ██  ██
+██      ██      ██   ██      ██ ██    ██ ██  ██ ██
+██      ███████ ██   ██ ███████  ██████  ██   ████
+*/
+
+#include "backprop.h"
+#include "pgmimage.h"
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+extern char *strcpy();
+extern void exit();
+
+int evaluate_performance(net, err) BPNN *net;
+double *err;
+{
+  double delta;
+
+  delta = net->target[1] - net->output_units[1];
+
+  *err = (0.5 * delta * delta);
+
+  /*** If the target unit is on... ***/
+  if (net->target[1] > 0.5) {
+
+    /*** If the output unit is on, then we correctly recognized me! ***/
+    if (net->output_units[1] > 0.5) {
+      return (1);
+
+      /*** otherwise, we didn't think it was me... ***/
+    } else {
+      return (0);
+    }
+
+    /*** Else, the target unit is off... ***/
+  } else {
+
+    /*** If the output unit is on, then we mistakenly thought it was me ***/
+    if (net->output_units[1] > 0.5) {
+      return (0);
+
+      /*** else, we correctly realized that it wasn't me ***/
+    } else {
+      return (1);
+    }
+  }
+  return 0;
+}
+
+/*** Computes the performance of a net on the images in the imagelist. ***/
+/*** Prints out the percentage correct on the image set, and the
+     average error between the target and the output units for the set. ***/
+int performance_on_imagelist(net, il, list_errors) BPNN *net;
+IMAGELIST *il;
+int list_errors;
+{
+  double err, val;
+  int i, n, j, correct;
+
+  err = 0.0;
+  correct = 0;
+  n = il->n;
+  if (n > 0) {
+    for (i = 0; i < n; i++) {
+
+      /*** Load the image into the input layer. **/
+      load_input_with_image(il->list[i], net);
+
+      /*** Run the net on this input. **/
+      bpnn_feedforward(net);
+
+      /*** Set up the target vector for this image. **/
+      load_target(il->list[i], net);
+
+      /*** See if it got it right. ***/
+      if (evaluate_performance(net, &val)) {
+        //匹配成功，计数器加1
+        correct++;
+      } else if (list_errors) {
+        printf("%s - outputs ", NAME(il->list[i]));
+
+        for (j = 1; j <= net->output_n; j++) {
+          printf("%.3f ", net->output_units[j]);
+        }
+
+        putchar('\n');
+      }
+      err += val;
+    }
+
+    err = err / (double)n;
+
+    if (!list_errors)
+      // 输出 匹配准确率 和 误差
+      printf("%g%%  %g \n", ((double)correct / (double)n) * 100.0, err);
+  } else {
+    if (!list_errors)
+      printf("0.0 0.0 ");
+  }
+
+  return 0;
+}
+
+// 评估图片集的匹配情况
+int result_on_imagelist(net, il, list_errors) BPNN *net;
+IMAGELIST *il;
+int list_errors;
+{
+  double err, val;
+  int i, n, j, correct;
+
+  err = 0.0;
+  correct = 0;
+
+  n = il->n; // 图片集元素个数
+  if (n > 0) {
+    for (i = 0; i < n; i++) {
+      /*** Load the image into the input layer. **/
+      load_input_with_image(il->list[i], net);
+
+      /*** Run the net on this input. **/
+      bpnn_feedforward(net);
+
+      /*** Set up the target vector for this image. **/
+      load_target(il->list[i], net);
+
+      // 输出图片的名称
+      printf("图片名称：%s\n", NAME(il->list[i]));
+
+      // 打印目标值
+      printf("目标值：");
+      for (j = 1; j <= net->output_n; j++) {
+        printf("%.3f ", net->target[j]);
+      }
+      printf("\n");
+
+      // 打印输出层单元输出值
+      printf("输出值：");
+      for (j = 1; j <= net->output_n; j++) {
+        printf("%.3f ", net->output_units[j]);
+      }
+      printf(" ");
+
+      /*** See if it got it right. ***/
+      if (evaluate_performance(net, &val)) {
+        correct++;
+        printf("识别成功 😎\n");
+      } else {
+        printf("识别失败 😡\n");
+      }
+
+      printf("\n");
+
+      err += val;
+    }
+
+    err = err / (double)n;
+
+    // 输出 准确率 和 平均误差
+    if (!list_errors)
+      printf("准确率：%g%%  平均误差：%g 👻\n\n",
+             ((double)correct / (double)n) * 100.0, err);
+  } else {
+    if (!list_errors)
+      printf("0.0 0.0 ");
+  }
+
+  return 0;
+}
+
+int backprop_face(trainlist, test1list, test2list, epochs, savedelta, netname,
+                  list_errors) IMAGELIST *trainlist,
+    *test1list, *test2list;
+int epochs, savedelta, list_errors;
+char *netname;
+{
+  IMAGE *iimg;
+  BPNN *net;
+  int train_n, epoch, i, imgsize;
+  double out_err, hid_err, sumerr;
+
+  train_n = trainlist->n;
+
+  /*** Read network in if it exists, otherwise make one from scratch ***/
+  if ((net = bpnn_read(netname)) == NULL) {
+    if (train_n > 0) {
+      printf("Creating new network '%s'\n", netname);
+      iimg = trainlist->list[0];
+      imgsize = ROWS(iimg) * COLS(iimg);
+      /* bthom ===========================
+        make a net with:
+          imgsize inputs, 4 hiden units, and 1 output unit
+    图片规模的输入层单元个数，4个隐藏层单元，1个输出层单元
+          */
+      net = bpnn_create(imgsize, 4, 1);
+    } else {
+      printf("Need some images to train on, use -t\n");
+      return -1;
+    }
+  }
+
+  if (epochs > 0) {
+    /*** 训练进行中（epochs次） ***/
+    printf("Training underway (going to %d epochs)\n", epochs);
+    /*** 每epochs次保存网络 ***/
+    printf("Will save network every %d epochs\n", savedelta);
+    fflush(stdout);
+  }
+
+  /*** 迭代前输出测试表现 ***/
+  /*** Print out performance before any epochs have been completed. ***/
+  printf("\n迭代前：\n");
+  printf("误差和：0.0\n");
+  printf("评估训练集的表现： ");
+  performance_on_imagelist(net, trainlist, 0);
+  printf("评估测试集1的表现：");
+  performance_on_imagelist(net, test1list, 0);
+  printf("评估测试集2的表现：");
+  performance_on_imagelist(net, test2list, 0);
+  printf("\n");
+  fflush(stdout);
+  if (list_errors) {
+    printf(
+        "\nFailed to classify the following images from the training set:\n");
+    performance_on_imagelist(net, trainlist, 1);
+    printf("\nFailed to classify the following images from the test set 1:\n");
+    performance_on_imagelist(net, test1list, 1);
+    printf("\nFailed to classify the following images from the test set 2:\n");
+    performance_on_imagelist(net, test2list, 1);
+  }
+
+  /************** 开始训练！ ****************************/
+  /************** Train it *****************************/
+  for (epoch = 1; epoch <= epochs; epoch++) {
+
+    // 输出迭代次数
+    printf("迭代次数： %d \n", epoch);
+    fflush(stdout);
+
+    sumerr = 0.0;
+    for (i = 0; i < train_n; i++) {
+
+      /** Set up input units on net with image i **/
+      load_input_with_image(trainlist->list[i], net);
+
+      /** Set up target vector for image i **/
+      load_target(trainlist->list[i], net);
+
+      /** Run backprop, learning rate 0.3, momentum 0.3 **/
+      /** 运行反向传播算法，学习速率0.3，冲量0.3 **/
+      bpnn_train(net, 0.3, 0.3, &out_err, &hid_err);
+
+      sumerr += (out_err + hid_err);
+    }
+    printf("误差和: %g \n", sumerr);
+
+    // 评估测试集，测试集1，测试集2 的表现
+    /*** Evaluate performance on train, test, test2, and print perf ***/
+    printf("评估训练集的表现： ");
+    performance_on_imagelist(net, trainlist, 0);
+    printf("评估测试集1的表现：");
+    performance_on_imagelist(net, test1list, 0);
+    printf("评估测试集2的表现：");
+    performance_on_imagelist(net, test2list, 0);
+    printf("\n");
+    fflush(stdout);
+
+    /*** Save network every 'savedelta' epochs ***/
+    if (!(epoch % savedelta)) {
+      bpnn_save(net, netname);
+    }
+  }
+  printf("\n");
+  fflush(stdout);
+
+  // 输出测试集中每张图片的匹配情况
+  printf("迭代结束后的匹配情况：\n\n");
+  printf("测试集1：\n\n");
+  result_on_imagelist(net, test1list, 0);
+  printf("测试集2：\n\n");
+  result_on_imagelist(net, test2list, 0);
+
+  /** Save the trained network **/
+  if (epochs > 0) {
+    bpnn_save(net, netname);
+  }
+  return 0;
+}
+
+void printusage(prog) char *prog;
+{
+  printf("USAGE: %s\n", prog);
+  printf("       -n <network file>\n");
+  printf("       [-e <number of epochs>]\n");
+  printf("       [-s <random number generator seed>]\n");
+  printf("       [-S <number of epochs between saves of network>]\n");
+  printf("       [-t <training set list>]\n");
+  printf("       [-1 <testing set 1 list>]\n");
+  printf("       [-2 <testing set 2 list>]\n");
+  printf("       [-T]\n");
+}
+
+int main(argc, argv) int argc;
+char *argv[];
+{
+  char netname[256], trainname[256], test1name[256], test2name[256];
+  IMAGELIST *trainlist, *test1list, *test2list;
+  int ind, epochs, seed, savedelta, list_errors;
+
+  // 种子
+  seed = 102194; /*** today's date seemed like a good default ***/
+
+  epochs = 100;
+  savedelta = 100;
+  list_errors = 0;
+  netname[0] = trainname[0] = test1name[0] = test2name[0] = '\0';
+
+  if (argc < 2) {
+    printusage(argv[0]);
+    exit(-1);
+  }
+
+  /*** Create imagelists ***/
+  trainlist = imgl_alloc();
+  test1list = imgl_alloc();
+  test2list = imgl_alloc();
+
+  /*** 扫描命令行 ***/
+  /*** Scan command line ***/
+  for (ind = 1; ind < argc; ind++) {
+
+    /*** 分析选择了那种操作 ***/
+    /*** Parse switches ***/
+    if (argv[ind][0] == '-') {
+      switch (argv[ind][1]) {
+      case 'n':
+        strcpy(netname, argv[++ind]);
+        break;
+      case 'e':
+        epochs = atoi(argv[++ind]);
+        break;
+      case 's':
+        seed = atoi(argv[++ind]);
+        break;
+      case 'S':
+        savedelta = atoi(argv[++ind]);
+        break;
+      case 't':
+        strcpy(trainname, argv[++ind]);
+        break;
+      case '1':
+        strcpy(test1name, argv[++ind]);
+        break;
+      case '2':
+        strcpy(test2name, argv[++ind]);
+        break;
+      case 'T':
+        list_errors = 1;
+        epochs = 0;
+        break;
+      default:
+        printf("Unknown switch '%c'\n", argv[ind][1]);
+        break;
+      }
+    }
+  }
+
+  /*** If any train, test1, or test2 sets have been specified, then
+       load them in. ***/
+  if (trainname[0] != '\0')
+    imgl_load_images_from_textfile(trainlist, trainname);
+  if (test1name[0] != '\0')
+    imgl_load_images_from_textfile(test1list, test1name);
+  if (test2name[0] != '\0')
+    imgl_load_images_from_textfile(test2list, test2name);
+
+  /*** If we haven't specified a network save file, we should... ***/
+  if (netname[0] == '\0') {
+    printf("%s: Must specify an output file, i.e., -n <network file>\n",
+           argv[0]);
+    exit(-1);
+  }
+
+  /*** Don't try to train if there's no training data ***/
+  if (trainname[0] == '\0') {
+    epochs = 0;
+  }
+
+  /*** 初始化神经网络包 ***/
+  /*** Initialize the neural net package ***/
+  bpnn_initialize(seed);
+
+  /*** 显示训练集，测试集1，测试集2中图片数量 ***/
+  /*** Show number of images in train, test1, test2 ***/
+  printf("%d images in training set\n", trainlist->n);
+  printf("%d images in test1 set\n", test1list->n);
+  printf("%d images in test2 set\n", test2list->n);
+
+  /*** If we've got at least one image to train on, go train the net ***/
+  backprop_face(trainlist, test1list, test2list, epochs, savedelta, netname,
+                list_errors);
+
+  exit(0);
+}
---
+++ neural-networks-for-face-recognition/src/sunglass-recognizer/imagenet.c
@@ -0,0 +1,91 @@
+/*
+ ******************************************************************
+ * HISTORY
+ * 15-Oct-94  Jeff Shufelt (js), Carnegie Mellon University
+ *      Prepared for 15-681, Fall 1994.
+ *
+ ******************************************************************
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include "pgmimage.h"
+#include "backprop.h"
+
+extern void exit();
+
+/* 避免使用0和1作为目标值的原因是sigmoid单元对于有限权值不能产生这样的输出,
+   如果我们企图用训练网络来准确匹配目标值0和1，梯度下降将会迫使权值无限增长。
+   值0.1和0.9是sigmoid函数在有限权值情况下可以完成的。
+*/
+#define TARGET_HIGH 0.9
+#define TARGET_LOW 0.1
+
+
+/*** This is the target output encoding for a network with one output unit.
+     It scans the image name, and if it's an image of me (js) then
+     it sets the target unit to HIGH; otherwise it sets it to LOW.
+     Remember, units are indexed starting at 1, so target unit 1
+     is the one to change....  ***/
+/***
+    目标输出编码
+***/
+
+// 太阳眼镜识别器
+int load_target(img, net)
+IMAGE *img;
+BPNN *net;
+{
+  int scale;
+  char userid[40], head[40], expression[40], eyes[40], photo[40];
+
+  userid[0] = head[0] = expression[0] = eyes[0] = photo[0] = '\0';
+
+  /*** scan in the image features ***/
+  sscanf(NAME(img), "%[^_]_%[^_]_%[^_]_%[^_]_%d.%[^_]",
+    userid, head, expression, eyes, &scale, photo);
+  //姓名     朝向     表情      眼镜    大小
+
+  // sunglasses == HIGH, open == LOW
+  if (!strcmp(eyes, "sunglasses"))
+  {
+    net->target[1] = TARGET_HIGH;  /* 戴眼镜, set target to HIGH */
+  } 
+  else 
+  {
+    net->target[1] = TARGET_LOW;   /* 没戴眼镜, set it to LOW */
+  }
+  return 0;
+}
+
+
+/***********************************************************************/
+/********* You shouldn't need to change any of the code below.   *******/
+/***********************************************************************/
+
+int load_input_with_image(img, net)
+IMAGE *img;
+BPNN *net;
+{
+  double *units;
+  int nr, nc, imgsize, i, j, k;
+
+  nr = ROWS(img);
+  nc = COLS(img);
+  imgsize = nr * nc;;
+  if (imgsize != net->input_n) {
+    printf("LOAD_INPUT_WITH_IMAGE: This image has %d pixels,\n", imgsize);
+    printf("   but your net has %d input units.  I give up.\n", net->input_n);
+    exit (-1);
+  }
+
+  units = net->input_units;
+  k = 1;
+  for (i = 0; i < nr; i++) {
+    for (j = 0; j < nc; j++) {
+      units[k] = ((double) img_getpixel(img, i, j)) / 255.0;
+      k++;
+    }
+  }
+  return 0;
+}
